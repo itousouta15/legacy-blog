b583c2e6af6bd58d8d89a5a0e8c884b7faff7a6f
itousouta15 <kuoray333@gmail.com>
2025-08-25 19:15:41 +0800

    更改圖標

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1746886558075";const preCache = ["/images/taichi.png","/img/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1746886558075";const preCache = ["/images/taichi.svg","/img/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
